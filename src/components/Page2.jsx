93b41b965f9b21e298c1784e6c5c7806b3a48ba7
Avadhut <mali.avadhut7@gmail.com>
2024-11-09 07:23:11 +0530

    Registration form edited

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -18,7 +18,7 @@ function Page2() {
                
             </div>
         </div>
-       <div className="box h-[130vh] md:h-[70vh] w-[80vw] flex flex-col md:flex-row gap-10">
+       <div className="box h-[115vh] md:h-[70vh] w-[80vw] flex flex-col md:flex-row gap-10">
             <div className="tux h-[30vh] md:h-full w-full md:w-1/2 rounded-xl bg-opacity-[20%]  bg-zinc-400 relative">
             <Canvas dpr={[1, 2]}>
                 <ambientLight intensity={1.4} />
